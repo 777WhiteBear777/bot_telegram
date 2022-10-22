2912d8c33b8b248cc75d785b1d29b3be49324fe8
WB_777 <antariuzz@gmail.com>
2022-10-22 21:24:17 +0300

    Вроде понятно, но чет не понятно

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,12 +2,14 @@ import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class main {
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    public static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
@@ -15,8 +17,34 @@ public class main {
             log.info("Registering bot...");
             telegramBotsApi.registerBot(new brobot());
         } catch (TelegramApiRequestException e) {
-            log.info("Failed to register bot(check internet connection / bot token or make sure only one instance of bot is running).");
+            log.error("Failed to register bot(check internet connection / bot token or make sure only one instance of bot is running).",e);
         }
         log.info("Telegram bot is ready to accept updates from user......");
     }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String textFromUser = update.getMessage().getText();
+
+            Long userId = update.getMessage().getChatId();
+            String userFirstName = update.getMessage().getFrom().getFirstName();
+
+            log.info("[{}, {}] : {}", userId, userFirstName, textFromUser);
+
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(userId.toString())
+                    .text("Hello, I've received your text: " + textFromUser)
+                    .build();
+            try {
+                this.sendApiMethod(sendMessage);
+            } catch (TelegramApiException e) {
+                log.error("Exception when sending message: ", e);
+            }
+        } else {
+            log.warn("Unexpected update from user");
+        }
+    }
+
 }
+
