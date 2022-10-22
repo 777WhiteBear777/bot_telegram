a59b24269aa77f9d4bb23a11b48827a9d359fb8b
WB_777 <antariuzz@gmail.com>
2022-10-22 21:53:30 +0300

    Some fixes

--- src/main/java/Brobot.java
+++ src/main/java/Brobot.java
@@ -1,27 +1,23 @@
 import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class main {
-    public static final Logger log = LoggerFactory.getLogger(Application.class);
+public class Brobot extends TelegramLongPollingBot {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) throws TelegramApiException {
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        try {
-            log.info("Registering bot...");
-            telegramBotsApi.registerBot(new brobot());
-        } catch (TelegramApiRequestException e) {
-            log.error("Failed to register bot(check internet connection / bot token or make sure only one instance of bot is running).",e);
-        }
-        log.info("Telegram bot is ready to accept updates from user......");
+    @Override
+    public String getBotUsername() {
+        return "Bro333bot";
     }
 
+    @Override
+    public String getBotToken() {
+        return "5711756410:AAHZF72d3DVjuLP5bl61tlmH88zAtnuttoA";
+    }
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -47,4 +43,3 @@ public class main {
     }
 
 }
-
